f6b6c038f5b21b12eea01eefd734c16d1176b5d1
Gustavo Schmaltz <guuhschmaltz@gmail.com>
2020-07-06 23:37:24 -0300

    design criado

---
+++ CrudSQL/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudSQL
+{
+    public class Usuario
+    {
+        public string Nome { get; set; }
+        public string Cidade { get; set; }
+        public string Cargo { get; set; }
+    }
+}
